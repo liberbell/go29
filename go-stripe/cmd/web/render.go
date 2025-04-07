4b7deffedd0c64d1ef03944a55816a6caba9f7ec
Liberbell <liberbell@gmail.com>
2025-04-08 08:37:16 +0900

    render3

--- go-stripe/cmd/web/render.go
+++ go-stripe/cmd/web/render.go
@@ -1,8 +1,12 @@
 package main
 
 type templateData struct {
-	StringMap map[string]string
-	IntMap    map[string]int
-	FloatMap map[string]float32
-	Data map[string]interface
+	StringMap       map[string]string
+	IntMap          map[string]int
+	FloatMap        map[string]float32
+	Data            map[string]interface{}
+	CSRFToken       string
+	Warning         string
+	Error           string
+	IsAuthenticated int
 }
