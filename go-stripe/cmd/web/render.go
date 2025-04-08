b5abf373309fa033e21de190b6555c7959589c01
Liberbell <liberbell@gmail.com>
2025-04-09 08:28:37 +0900

    render2

--- go-stripe/cmd/web/render.go
+++ go-stripe/cmd/web/render.go
@@ -3,6 +3,7 @@ package main
 import (
 	"embed"
 	"html/template"
+	"net/http"
 )
 
 type templateData struct {
@@ -21,3 +22,7 @@ type templateData struct {
 var functions = template.FuncMap{}
 
 var templateFS embed.FS
+
+func (app *application) addDefaultData(td *templateData, r *http.Request) *templateData {
+	return td
+}
