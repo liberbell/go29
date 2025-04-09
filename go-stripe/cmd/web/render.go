82ad2ea967383c30c99f1d2a99b0ea80d54576f9
Liberbell <liberbell@gmail.com>
2025-04-10 08:23:52 +0900

    render4

--- go-stripe/cmd/web/render.go
+++ go-stripe/cmd/web/render.go
@@ -58,6 +58,6 @@ func (app *application) parseTemplate(partials []string, page, templateData stri
 	}
 
 	if len(partials) > 0 {
-		t, err = template.New(fmt.Sprintf("%s.page.tmpl", page))
+		t, err = template.New(fmt.Sprintf("%s.page.tmpl", page)).Funcs(functions).ParseFS(templateFS, "templates/base.layout.tmpl")
 	}
 }
