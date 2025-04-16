765a89e3ce24628869f92bbb9a265880cb150328
Liberbell <liberbell@gmail.com>
2025-04-17 08:30:08 +0900

    card5

--- go-stripe/internal/card/card.go
+++ go-stripe/internal/card/card.go
@@ -49,6 +49,14 @@ func cardErrorMessage(code stripe.ErrorCode) string {
 		msg = "Your card was declined."
 	case stripe.ErrorCodeExpiredCard:
 		msg = "Your card is expired."
+	case stripe.ErrorCodeIncorrectCVC:
+		msg = "Incorrect CVC code."
+	case stripe.ErrorCodeIncorrectZip:
+		msg = "Incorrect ZIP code."
+	case stripe.ErrorCodeAmountTooLarge:
+		msg = "The amount is too large to charge to your card."
+	case stripe.ErrorCodeAmountTooSmall:
+		msg = "The amount is too small to charge to your card."
 	default:
 		msg = "Your card was declined."
 	}
