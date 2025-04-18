2719933a7c07bed31036c513b620f1c8af04910a
Liberbell <liberbell@gmail.com>
2025-04-18 12:53:27 +0900

    routes-api2

--- go-stripe/cmd/api/routes-api.go
+++ go-stripe/cmd/api/routes-api.go
@@ -4,10 +4,14 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
 )
 
 func (app *application) routes() http.Handler {
 	mux := chi.NewRouter()
+	mux.Use(cors.Handler(cors.Options{
+		AllowedOrigins: []string("https://*"),
+	}))
 
 	retrun mux
 }
