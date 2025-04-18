a5656b4999fac5b36ff196c467ece9c092f80bd5
Liberbell <liberbell@gmail.com>
2025-04-18 12:43:04 +0900

    api6

--- go-stripe/cmd/api/api.go
+++ go-stripe/cmd/api/api.go
@@ -44,7 +44,7 @@ func (app application) serve() error {
 		WriteTimeout:      5 * time.Second,
 	}
 
-	app.infoLog.Println(fmt.Sprintf("Starting HTTP Server in %s mode on port %d", app.config.env, app.config.port))
+	app.infoLog.Println(fmt.Sprintf("Starting Back end Server in %s mode on port %d", app.config.env, app.config.port))
 
 	return srv.ListenAndServe()
 }
@@ -67,5 +67,10 @@ func main() {
 		config:   cfg,
 		infoLog:  infoLog,
 		errorLog: errorLog,
+	}
+
+	err := app.serve()
+	if err != nil {
+		log.Fatal(err)
 	}
 }
