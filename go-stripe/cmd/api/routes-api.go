1007101d0092eec8742a57109ea96c3380d295a7
Liberbell <liberbell@gmail.com>
2025-04-19 13:52:11 +0900

    routes1

--- go-stripe/cmd/api/routes-api.go
+++ go-stripe/cmd/api/routes-api.go
@@ -10,12 +10,14 @@ import (
 func (app *application) routes() http.Handler {
 	mux := chi.NewRouter()
 	mux.Use(cors.Handler(cors.Options{
-		AllowedOrigins: []string("https://*", "http://*"),
-		AllowedMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-		AllowedHeaders: []string{"Accept", "Authorization", "Contetent-Type", "X-CSRF-Token", "X-Requested-With"},
+		AllowedOrigins:   []string("https://*", "http://*"),
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Contetent-Type", "X-CSRF-Token", "X-Requested-With"},
 		AllowCredentials: false,
-		MaxAge: 300,
+		MaxAge:           300,
 	}))
 
-	retrun mux
+	mux.Get("/api/payment-intent", app.GetPyamentIntent)
+
+	return mux
 }
