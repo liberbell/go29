a744b6afe0b0a1aa92b1aae90cf1c17eddbc2be5
Liberbell <liberbell@gmail.com>
2025-04-19 13:41:00 +0900

    handlers-api2

--- go-stripe/cmd/api/handlers-api.go
+++ go-stripe/cmd/api/handlers-api.go
@@ -1,6 +1,12 @@
 package main
 
 type stripePayload struct {
-	Currency string `json:"currency`
+	Currency string `json:"currency"`
 	Amount   string `json:"amount"`
+}
+
+type jsonResponse struct {
+	Ok      bool   `json:"ok"`
+	Message string `json:"message"`
+	Content string `json:"content"`
 }
