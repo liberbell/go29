320b7f753af10418efa757af799aa0b92a7c0d3a
Liberbell <liberbell@gmail.com>
2025-04-30 08:12:00 +0900

    api1

--- go-stripe/cmd/api/api.go
+++ go-stripe/cmd/api/api.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"fmt"
 	"log"
@@ -45,6 +46,20 @@ func (app application) serve() error {
 	return srv.ListenAndServe()
 }
 
+func OpenDB(dsn string) (*sql.DB, error) {
+	// db, err := sql.Open("mysql", dsn)
+	db, err := sql.Open("mysql", "james:secret@/widgets?parseTime=true&tls=false")
+	if err != nil {
+		return nil, err
+	}
+	err = db.Ping()
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+	return db, nil
+}
+
 func main() {
 	var cfg config
 	flag.IntVar(&cfg.port, "port", 4001, "Server port to listen on")
@@ -59,6 +74,12 @@ func main() {
 
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	conn, err := OpenDB(cfg.db.dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+	defer conn.Close()
 
 	app := &application{
 		config:   cfg,
