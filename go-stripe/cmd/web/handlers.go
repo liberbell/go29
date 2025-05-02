62c7e8e89e73c3e302a175e9cd4110fa3a173ded
Liberbell <liberbell@gmail.com>
2025-05-02 15:10:08 +0900

    handlers1

--- go-stripe/cmd/web/handlers.go
+++ go-stripe/cmd/web/handlers.go
@@ -7,7 +7,7 @@ func (app *application) VirtualTerminal(w http.ResponseWriter, r *http.Request) 
 	stringMap["publishable_key"] = app.config.stripe.key
 	if err := app.renderTemplate(w, r, "terminal", &templateData{
 		StringMap: stringMap,
-	}); err != nil {
+	}, "stripe-js"); err != nil {
 		app.errorLog.Println(err)
 	}
 }
