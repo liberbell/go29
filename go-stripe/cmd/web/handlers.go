216c86376dcbbb9fac3f9c7fa4c9d907f0fae5a3
Liberbell <liberbell@gmail.com>
2025-05-08 07:52:06 +0900

    handlers1

--- go-stripe/cmd/web/handlers.go
+++ go-stripe/cmd/web/handlers.go
@@ -1,8 +1,10 @@
 package main
 
 import (
-	"myapp/internal/models"
 	"net/http"
+	"strconv"
+
+	"github.com/go-chi/chi"
 )
 
 func (app *application) VirtualTerminal(w http.ResponseWriter, r *http.Request) {
@@ -42,14 +44,22 @@ func (app *application) PaymentSucceeded(w http.ResponseWriter, r *http.Request)
 }
 
 func (app *application) ChargeOnce(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+	widgetID, _ := strconv.Atoi(id)
 
-	widget := models.Widget{
-		ID:             1,
-		Name:           "Custom Widget",
-		Description:    "A very nice widget",
-		InventoryLevel: 10,
-		Price:          1000,
+	widgetID, err := app.DB.GetWidget(widgetID)
+	if err != nil {
+		app.errorLog.Println(err)
+		return
 	}
+
+	// widget := models.Widget{
+	// 	ID:             1,
+	// 	Name:           "Custom Widget",
+	// 	Description:    "A very nice widget",
+	// 	InventoryLevel: 10,
+	// 	Price:          1000,
+	// }
 	data := make(map[string]interface{})
 	data["widget"] = widget
 
