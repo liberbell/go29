d0f81f62dc839c6f58aba3fde60e2aef9be151b5
Liberbell <liberbell@gmail.com>
2025-05-15 08:14:40 +0900

    gostripe1

--- go-stripe/cmd/web/routes.go
+++ go-stripe/cmd/web/routes.go
@@ -9,7 +9,7 @@ import (
 func (app *application) routes() http.Handler {
 	mux := chi.NewRouter()
 
-	mux.Get("/home", app.Home)
+	mux.Get("/", app.Home)
 	mux.Get("/virtual-terminal", app.VirtualTerminal)
 	mux.Post("/payment-succeeded", app.PaymentSucceeded)
 
