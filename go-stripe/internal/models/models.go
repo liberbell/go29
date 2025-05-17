23654a1d5244572bdfa3181ed5f760850afa69ba
Liberbell <liberbell@gmail.com>
2025-05-17 14:42:39 +0900

    models1

--- go-stripe/internal/models/models.go
+++ go-stripe/internal/models/models.go
@@ -156,14 +156,15 @@ func (m *DBModel) InsertOrder(order Order) (int, error) {
 
 	stmt := `
 		INSERT INTO orders 
-			(widget_id, transaction_id, status_id, quantity, amount, created_at, updated_at)
-		values (?, ?, ?, ?, ?, ?, ?)
+			(widget_id, transaction_id, status_id, quantity, customer_id, amount, created_at, updated_at)
+		values (?, ?, ?, ?, ?, ?, ?, ?)
 		`
 	result, err := m.DB.ExecContext(ctx, stmt,
 		order.WidgetID,
 		order.TransactionID,
 		order.StatusID,
 		order.Quantity,
+		order.CustomerID,
 		order.Amount,
 		time.Now(),
 		time.Now(),
