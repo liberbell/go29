4eec14698f4def131ea03a4441c3d6c77b98ceaa
Liberbell <liberbell@gmail.com>
2025-05-18 14:04:15 +0900

    models3

--- go-stripe/internal/models/models.go
+++ go-stripe/internal/models/models.go
@@ -66,6 +66,7 @@ type Transaction struct {
 	ExpiryMonth         int       `json:"expiry_month"`
 	ExpiryYear          int       `json:"expiry_year"`
 	PaymentIntent       string    `json:"payment_intent"`
+	PaymentMethod       string    `json:"payment_method"`
 	BankReturnCode      string    `json:"bank_return_code"`
 	TransactionStatusID int       `json:"transaction_status_id"`
 	CreatedAt           time.Time `json:"-"`
@@ -127,8 +128,8 @@ func (m *DBModel) InsertTransaction(txn Transaction) (int, error) {
 
 	stmt := `
 		INSERT INTO transactions 
-			(amount, currency, last_four, bank_return_code, expiry_month, expiry_year, transaction_status_id, created_at, updated_at)
-		values (?, ?, ?, ?, ?, ?, ?, ?, ?)
+			(amount, currency, last_four, bank_return_code, expiry_month, expiry_year, payment_intent, payment_method, transaction_status_id, created_at, updated_at)
+		values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
 		`
 	result, err := m.DB.ExecContext(ctx, stmt,
 		txn.Amount,
@@ -137,6 +138,8 @@ func (m *DBModel) InsertTransaction(txn Transaction) (int, error) {
 		txn.BankReturnCode,
 		txn.ExpiryMonth,
 		txn.ExpiryYear,
+		txn.PaymentIntent,
+		txn.PaymentMethod,
 		txn.TransactionStatusID,
 		time.Now(),
 		time.Now(),
